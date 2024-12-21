22c4370bf25eca742d2b7aca2846efcb02be4974
Shubhangam Tyagi <tyagishubhangam0090@gmail.com>
2024-12-21 10:24:12 +0530

    Enroll Profile to Course Added and Course Data fill api added

---
+++ src/main/java/com/naval_innovators/your_exam_sathi/auth_service/controller/ProfileController.java
@@ -0,0 +1,30 @@
+package com.naval_innovators.your_exam_sathi.auth_service.controller;
+
+import com.naval_innovators.your_exam_sathi.auth_service.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+@RequiredArgsConstructor
+@RequestMapping("/api/profile/{profileId}")
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    @PostMapping("/course/{courseId}/enroll")
+    public ResponseEntity<Map<String, Boolean>> enrollToCourse(@PathVariable long profileId, @PathVariable long courseId) {
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("enrolled", false);
+        if (profileService.enrollToCourse(profileId,courseId)){
+            response.put("enrolled", true);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/naval_innovators/your_exam_sathi/auth_service/models/Profile.java
+++ src/main/java/com/naval_innovators/your_exam_sathi/auth_service/models/Profile.java
@@ -1,21 +1,12 @@
+
 package com.naval_innovators.your_exam_sathi.auth_service.models;
+
 import java.time.LocalDate;
+import java.util.Set;
 
 import com.naval_innovators.your_exam_sathi.auth_service.models.enums.Gender;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,44 +19,50 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "profile_tbl")
 public class Profile {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(nullable = false)
+
+	@Column(name = "first_name", nullable = false)
 	private String firstName;
 
+	@Column(name = "last_name")
 	private String lastName;
 
-
-	@Column(length = 512)
+	@Column(name = "avatar_url", length = 512)
 	private String avatarUrl;
 
-	@Column(nullable = false)
+	@Column(name = "is_verified", nullable = false)
 	private boolean isVerified;
 
-	@Column(nullable = false)
+	@Column(name = "is_premium", nullable = false)
 	private boolean isPremium;
 
 	@Enumerated(EnumType.STRING)
-	@Column(length = 20)
+	@Column(name = "gender", length = 20)
 	private Gender gender;
 
 	@Column(name = "date_of_birth")
 	private LocalDate dateOfBirth;
-	
 
 	@OneToOne
-	@JoinColumn(name ="user_id")
+	@JoinColumn(name = "user_id")
 	private User user;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "college_id")
 	private College college;
-	
+
 	@ManyToOne
-    @JoinColumn(name = "branch_id", nullable = false)
-    private Branch branch;
-	
-	// user profile
+	@JoinColumn(name = "branch_id", nullable = false)
+	private Branch branch;
+
+	@ManyToMany
+	@JoinTable(
+			name = "profile_courses", // Join table name
+			joinColumns = @JoinColumn(name = "profile_id"), // Foreign key for Profile
+			inverseJoinColumns = @JoinColumn(name = "course_id") // Foreign key for Course
+	)
+	private Set<Course> courses;
 }
