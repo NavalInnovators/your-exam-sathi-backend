1c970c816e5c2d35e389f0097b94b48474572192
kush-prog <comedykid041@gmail.com>
2024-12-24 14:31:12 +0530

    Update the feedback endpoints

--- src/main/java/com/naval_innovators/your_exam_sathi/auth_service/controller/FeedbackController.java
+++ src/main/java/com/naval_innovators/your_exam_sathi/auth_service/controller/FeedbackController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/feedback")
+@RequestMapping("/api/user/feedback")
 public class FeedbackController {
 
     @Autowired
