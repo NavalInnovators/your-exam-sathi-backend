cb51047d9a1e066dc17b084c57ee0976ab12ad8d
kush-prog <comedykid041@gmail.com>
2024-12-25 19:46:03 +0530

    Star feature added in feedback

--- src/main/java/com/naval_innovators/your_exam_sathi/auth_service/repository/FeedbackRepository.java
+++ src/main/java/com/naval_innovators/your_exam_sathi/auth_service/repository/FeedbackRepository.java
@@ -2,6 +2,10 @@ package com.naval_innovators.your_exam_sathi.auth_service.repository;
 
 import com.naval_innovators.your_exam_sathi.auth_service.models.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
 }
